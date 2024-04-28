9acdc1942b5e5b43752b7ca7de6add3d2e292eb9
XzeraSpartan <143136863+XzeraSpartan@users.noreply.github.com>
2024-04-28 01:39:34 -0700

    fetch data changes

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
-from controllers import comparison_controller
+from controllers import comparison_controller, book_controller
+
+from supabase import create_client, Client
+
 
 app = Flask(__name__)
 load_dotenv()  # Load environment variables
 
-# Register the routes
 app.register_blueprint(comparison_controller.bp)
+
+# Register the routes
+app.register_blueprint(book_controller.bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
