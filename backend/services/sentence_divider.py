a108a9a646b91121f12a185e163420d682905b8b
sainaveen1117 <54936905+sainaveen1117@users.noreply.github.com>
2024-04-28 04:47:45 -0700

    edited imports

--- backend/services/sentence_divider.py
+++ backend/services/sentence_divider.py
@@ -2,7 +2,11 @@ import nltk
 from nltk.tokenize import sent_tokenize
 
 # Ensure that the necessary NLTK data is downloaded
+<<<<<<< Updated upstream
 # nltk.download('punkt')
+=======
+nltk.download('punkt')
+>>>>>>> Stashed changes
 def divide(text):
     return list(sent_tokenize(text))
 
